827c87aae63aa545175b0ba005ceae36f5f22caf
unknown <bsk28042005@gmail.com>
2025-01-20 04:51:20 +0530

    bhavesh bhadwa chutia

--- calculator.py
+++ calculator.py
@@ -1,3 +1,11 @@
 # bhavesh-multi,divide
 #addition subtraction -himanshu
 #harsh -power
+
+def multi(a,b) :
+    return print(f"Multiplication = {a*b}")
+
+def divide(a,b) : 
+        return print(f"Division = {a/b}")
+
+
