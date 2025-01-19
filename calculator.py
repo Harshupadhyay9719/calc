b8c3235833f0eb273ed405aa1a1977550efb020d
Harshupadhyay9719 <upadhyayhb2006@gmail.com>
2025-01-20 05:26:10 +0530

    badvesh ka sahi kiya

--- calculator.py
+++ calculator.py
@@ -2,11 +2,13 @@
 #addition subtraction -himanshu
 #harsh -power
 add_minus
-def add(a,b) :
-    return print(f"Addition of {a} and {b} is {a+b}")
+def add():
+     a=float(input("enter the first number : "))
+     b=float(input("enter the second number : "))
+     print(f"Addition of {a} and {b} is {a+b}")
  
 def sub(a,b) :
-    return print(f"Subtraction of {a} and {b} is {a-b}")
+     print(f"Subtraction of {a} and {b} is {a-b}")
 
 
 def power():
@@ -25,12 +27,17 @@ def power():
         for i in range(0,int(b)):
             c=c*a
         print(a)
-num=int(input("enter 1 if you want to ADDITION OR SUBTRACTION :: \nenter 2 if you want to MULTIPLICATION OR DIVISION ::\nenter 3 if you want to find POWER ::  \n"))
-if(num==3):
+num=int(input("enter 1 if you want to ADDITION and 2 for SUBTRACTION :: \nenter 3 if you want to MULTIPLICATION and 4 for DIVISION ::\nenter 5 if you want to find POWER ::  \n"))
+if(num==5):
     power()
-# elif(num==1):
-#     sub(1)
-# elif(num==2):
-#     bhavesh(2):
-# else()
+elif(num==1):
+     add()
+elif(num==2):
+    sub()
+elif(num==3):
+    multi()
+elif(num==4):
+    divide()
+else:
+    print("BUND MARAO")
  
