19e9d0f049e5eae12780449fa75e69364dc30634
Hemanshu-29 <phogat.hemanshu.502@gmail.com>
2025-01-20 04:40:55 +0530

    addition and subtraction

--- calculator.py
+++ calculator.py
@@ -1,3 +1,17 @@
 # bhavesh-multi,divide
 #addition subtraction -himanshu
 #harsh -power
+n1 = int(input("enter first number to perform addition/subtraction :"))
+n2 = int(input('Enter second number to add/subtract from : '))
+
+print('Enter  : \n  "A" for Addition \n "S" for subtraction  ')
+
+
+def add(n)
+input = str(input())
+if input == 'a' or input == "A" :
+    print(f'the sum of the numbers is : {n1+n2}')
+elif input == "S" or input =='s' :
+    print(f'The difference of the numbers is : {n1-n2}')
+
+print('thank you')
